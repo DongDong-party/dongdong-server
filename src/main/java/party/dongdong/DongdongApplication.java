183f4437630a1bcce86c2a190fac7771c6385cc1
JeongWon Liee <khjvvv7@gmail.com>
2020-07-23 17:04:08 +0900

    init: init project
    - hello world

---
+++ src/main/java/party/dongdong/DongdongApplication.java
@@ -0,0 +1,13 @@
+package party.dongdong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongdongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongdongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/party/dongdong/DongdongApplicationTests.java
@@ -0,0 +1,13 @@
+package party.dongdong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongdongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
