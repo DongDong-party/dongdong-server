61f3ff8e58ffd736f3bfcdc5b39febe7422547ce
JeongWon Liee <khjvvv7@gmail.com>
2020-07-30 16:47:14 +0900

    fix: store list query use fetch join

--- src/main/java/party/dongdong/repository/StoreQueryRepository.java
+++ src/main/java/party/dongdong/repository/StoreQueryRepository.java
@@ -1,11 +1,13 @@
 package party.dongdong.repository;
 
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
-import party.dongdong.domain.*;
-import party.dongdong.dto.StoreListDto;
+
+import party.dongdong.domain.Store;
+import party.dongdong.domain.QCategory;
+import party.dongdong.domain.QStore;
+import party.dongdong.domain.QStoreImage;
 
 
 import java.util.List;
@@ -23,10 +25,15 @@ public class StoreQueryRepository extends QuerydslRepositorySupport {
     public List<Store> findAllDesc() {
         QStore store = QStore.store;
         QStoreImage storeImage = QStoreImage.storeImage;
+        QCategory category = QCategory.category;
 
         List<Store> stores = query.select(store)
                                 .from(store)
+                                .innerJoin(store.category, category)
+                                .fetchJoin()
                                 .leftJoin(store.images, storeImage)
+                                .fetchJoin()
+                                .leftJoin(storeImage.image)
                                 .fetchJoin()
                                 .distinct()
                                 .fetch();
