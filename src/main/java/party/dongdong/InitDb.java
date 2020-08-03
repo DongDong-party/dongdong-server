92aed82d890974d6f6678fc098b53a8b08064dd4
JeongWon Liee <khjvvv7@gmail.com>
2020-08-03 17:12:21 +0900

    config: deprecate init db

--- src/main/java/party/dongdong/InitDb.java
+++ src/main/java/party/dongdong/InitDb.java
@@ -1,40 +1,40 @@
-package party.dongdong;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-import party.dongdong.domain.Category;
-
-import javax.annotation.PostConstruct;
-import javax.persistence.EntityManager;
-
-@RequiredArgsConstructor
-@Component
-public class InitDb {
-
-    private final InitService initService;
-
-    @PostConstruct
-    public void init() {
-        initService.init();
-    }
-
-    @RequiredArgsConstructor
-    @Component
-    @Transactional
-    static class InitService {
-
-        private final EntityManager em;
-
-        public void init() {
-            Category category1 = new Category();
-            Category category2 = new Category();
-            category1.setName("맛집");
-            category2.setName("카페");
-            em.persist(category1);
-            em.persist(category2);
-        }
-
-    }
-
-}
+//package party.dongdong;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//import party.dongdong.domain.Category;
+//
+//import javax.annotation.PostConstruct;
+//import javax.persistence.EntityManager;
+//
+//@RequiredArgsConstructor
+//@Component
+//public class InitDb {
+//
+//    private final InitService initService;
+//
+//    @PostConstruct
+//    public void init() {
+//        initService.init();
+//    }
+//
+//    @RequiredArgsConstructor
+//    @Component
+//    @Transactional
+//    static class InitService {
+//
+//        private final EntityManager em;
+//
+//        public void init() {
+//            Category category1 = new Category();
+//            Category category2 = new Category();
+//            category1.setName("맛집");
+//            category2.setName("카페");
+//            em.persist(category1);
+//            em.persist(category2);
+//        }
+//
+//    }
+//
+//}
